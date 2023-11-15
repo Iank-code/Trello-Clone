b8e47b293dafc92e56423c62a332d8cace7cf2cd
Ian Kamau <kian99564@gmail.com>
2023-11-15 17:03:02 +0300

    Developing Marketing:Landing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import { Medal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 
 export default function MarketingPage() {
   return (
     <div className="flex items-center justify-center flex-col">
       <div className="flex flex-col justify-center items-center">
-        <div className="mb-4 items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
           No 1 task management
         </div>
@@ -14,9 +17,19 @@ export default function MarketingPage() {
         </h1>
 
         <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
-            work forward.
+          work forward.
         </div>
       </div>
+
+      <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+        Collaborate, manage projects, and reach new productivity peaks. From
+        high rises to the home office, the way your team works is unique -
+        accomplish it all with Taskify
+      </div>
+
+      <Button>
+        <Link href="/sign-up">Get Taskify for free</Link>
+      </Button>
     </div>
   );
 }
